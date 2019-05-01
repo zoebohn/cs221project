d7a45dafbc5266af257c8e8ba59a7a7160722c9b
Zoë Bohn <zbohn2@Zoes-MBP-3.attlocal.net>
2019-04-30 18:45:32 -0700

    added csvs with labels for tweet handles

--- tweet_collector.py
+++ tweet_collector.py
@@ -1,27 +1,27 @@
 import subprocess
 import csv
 
-house_handles = "house_handles.txt"
-senate_handles = "senator_handles.txt"
+house_handles = "house_handles.csv"
+senate_handles = "senate_handles.csv"
 
 def load_congress_handles():
     handles = []
     with open(house_handles) as csv_file:
-        csv_reader = csv.reader(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
-            handles += row
+            handles.append(row[1])
 
     with open(senate_handles) as csv_file:
-        csv_reader = csv.reader(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
-            handles += row
+            handles.append(row[1])
     
     return handles
 
 def scrape_tweets(congress_member_handle):
     
     command = "/usr/local/bin/twitterscraper"
-    filename = "Tweets/" + congress_member_handle + ".json"
+    filename = "Tweets_Final/" + congress_member_handle + ".json"
     # /usr/local/bin/twitterscraper -u amyklobuchar --begindate 2018-01-01 --output=tweets.json
     subprocess.call([command, "-u", congress_member_handle, "--begindate",
         "2017-01-01", "--output", filename])
